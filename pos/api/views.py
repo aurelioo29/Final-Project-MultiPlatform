ef5b6a1b6c7b4f66ab93ec89e806f7c9cf17fcee
aurelioo29 <aureliolucio5@gmail.com>
2024-06-02 21:54:37 +0700

    remove Info-Rent API is no longer used

--- pos/api/views.py
+++ pos/api/views.py
@@ -5,7 +5,6 @@ from rest_framework import permissions
 from pos_app.models import User, StatusModel
 from api.serializers import RegiserUserSerializer
 # modules main view
-from api.main_view.info_rent import InfoRentListApiView, InfoRentDetailApiView
 from api.main_view.payment import PaymentListApiView, PaymentDetailApiView
 from api.main_view.customer import CustomerListApiView, CustomerDetailApiView
 from api.main_view.car_category import CarCategoryListApiView, CarCategoryDetailApiView
@@ -14,7 +13,6 @@ from api.main_view.car import CarListApiView, CarDetailApiView
 from api.auth_view.car import CarView
 from api.auth_view.car_category import CarCategoryView
 from api.auth_view.customer import CustomerView
-from api.auth_view.info_rent import InfoRentView
 from api.auth_view.payment import PaymentView
 # modules filter car
 from api.filter_view.filter import CarFilterApi
